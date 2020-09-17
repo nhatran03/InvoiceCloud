242e529c67e91cba0836dd894c4008bb5fb37553
nhatran03 <tranthanhnha84@gmail.com>
2020-09-17 17:09:28 -0500

    second commit

---
+++ InvoiceCloud/InvoiceCloud/Domain/Entities/Auditing/IHasModificationTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceCloud.Domain.Entities.Auditing
+{
+	public interface IHasModificationTime
+	{
+		DateTime? LastModificationTime { get; set; }
+	}
+}
---
+++ InvoiceCloud/InvoiceCloud/Domain/Entities/Auditing/ImodificationAudited.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceCloud.Domain.Entities.Auditing
+{
+	public interface IModificationAudited : IHasModificationTime
+	{
+		long? LastModifierUserId { get; set; }
+	}
+
+	public interface IModificationAudited<TUser> : IModificationAudited
+		where TUser: IEntity<long>
+	{
+		TUser LastModifierUser { get; set; }
+	}
+}
